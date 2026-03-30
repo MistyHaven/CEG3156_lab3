603ff791cc560b55a1a52b1cd76abb6f21676087
Alina SH <157328243+MistyHaven@users.noreply.github.com>
2026-03-30 16:40:29 -0400

    Add files via upload

---
+++ vhdl/equalsChecker.vhd
@@ -0,0 +1,38 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity equalsChecker is
+  generic ( n : integer := 4 );
+  port (
+    x , y: in std_logic_vector(n-1 downto 0);
+    o: out std_logic
+ );
+ end equalsChecker;
+
+architecture structural of equalsChecker is
+  signal i_y : std_logic_vector(n-1 downto 0);
+
+  
+  component inverter_1bit is 
+    port (
+      a, s: in std_logic;
+      o : out std_logic
+    );
+	end component;
+	
+	component isZero is 
+    port (
+		i : in std_logic_vector(n-1 downto 0);
+		o : out std_logic
+	 );
+	 end component;
+begin
+  invert_y: for i in n-1 downto 0 generate
+    noty : inverter_1bit
+    port map ( y(i), x(i), i_y(i));
+  end generate;
+  
+  isEqual : isZero 
+  port map(i_y, o);
+
+end structural;
