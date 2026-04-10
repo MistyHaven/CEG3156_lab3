e70c0fec547efbcf9ff065e273af62983f76f97f
Kai R <stfourrtup@gmail.com>
2026-04-10 15:09:07 -0400

    mem wb buffer

--- vhdl/top-level-components/buffer4_MemWb.vhdl
+++ vhdl/top-level-components/buffer4_MemWb.vhdl
@@ -3,20 +3,18 @@ use ieee.std_logic_1164.all;
 
 entity buffer_MemWb is 
   port (
-    i_clk: in std_logic;
+    i_clk: std_logic;
     i_flush: in std_logic;
   
     i_RegReadData1 : in std_logic_vector(32-1 downto 0);
-    i_MemAddress : in std_logic_vector(32-1 downto 0);
+    i_MemAddress : out std_logic_vector(32-1 downto 0);
     i_CtrlWb_RegWrite, i_CtrlWb_MemToReg: in std_logic; --2
-
 
     o_RegReadData1 : out std_logic_vector(32-1 downto 0);
     o_MemAddress : out std_logic_vector(32-1 downto 0);
     o_CtrlWb_RegWrite, o_CtrlWb_MemToReg: out std_logic
-    
   );
-end buffer_MemWb;
+end buffer_MemWb; 
 
 architecture rtl of buffer_MemWb is
   component regPIPO_nBit is
@@ -46,5 +44,9 @@ buffer_in(63 downto 32) <= i_MemAddress;
 buffer_in(64) <= i_CtrlWb_RegWrite;
 buffer_in(65) <= i_CtrlWb_MemToReg;
 
+o_RegReadData1 <= buffer_out(31 downto 0); 
+o_MemAddress <= buffer_out(63 downto 32); 
+o_CtrlWb_RegWrite <= buffer_out(64); 
+o_CtrlWb_MemToReg <= buffer_out(65);
 end architecture;
 
