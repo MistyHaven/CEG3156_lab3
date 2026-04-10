0a96095839e12054e464ac819176b2bc09e3ee14
Kai R <stfourrtup@gmail.com>
2026-04-10 14:52:03 -0400

    mem wb buffer

--- vhdl/top-level-components/buffer4_MemWb.vhdl
+++ vhdl/top-level-components/buffer4_MemWb.vhdl
@@ -3,9 +3,17 @@ use ieee.std_logic_1164.all;
 
 entity buffer_MemWb is 
   port (
-    clk: in std_logic;
-    flush: in std_logic
+    i_clk: in std_logic;
+    i_flush: in std_logic;
+  
+    i_RegReadData1 : in std_logic_vector(32-1 downto 0);
+    i_MemAddress : in std_logic_vector(32-1 downto 0);
+    i_CtrlWb_RegWrite, i_CtrlWb_MemToReg: in std_logic; --2
 
+
+    o_RegReadData1 : out std_logic_vector(32-1 downto 0);
+    o_MemAddress : out std_logic_vector(32-1 downto 0);
+    o_CtrlWb_RegWrite, o_CtrlWb_MemToReg: out std_logic
     
   );
 end buffer_MemWb;
@@ -20,19 +28,23 @@ architecture rtl of buffer_MemWb is
       o : out std_logic_vector(bit_width-1 downto 0)
     );
   end component;
-  signal buffer_in : std_logic_vector(32 downto 0); -- DEFINE TOTAL BIT WIDTH HERE
-  signal buffer_out : std_logic_vector(32 downto 0); -- DEFINE TOTAL BIT WIDTH HERE
+  signal buffer_in : std_logic_vector(66-1 downto 0); -- DEFINE TOTAL BIT WIDTH HERE
+  signal buffer_out : std_logic_vector(66-1 downto 0); -- DEFINE TOTAL BIT WIDTH HERE
 begin
-
-
   buf: regPIPO_nBit
-  generic map( bit_width => 32) -- DEFINE TOTAL BIT WIDTH THERE
+  generic map( bit_width => 66) -- DEFINE TOTAL BIT WIDTH THERE
   port map (
-    clock => clk,
-    reset => flush,
-    load => clk,
+    clock => i_clk,
+    reset => i_flush,
+    load => i_clk,
     d => buffer_in,
     o => buffer_out
   );
+  
+buffer_in(31 downto 0) <= i_RegReadData1;
+buffer_in(63 downto 32) <= i_MemAddress;
+buffer_in(64) <= i_CtrlWb_RegWrite;
+buffer_in(65) <= i_CtrlWb_MemToReg;
+
 end architecture;
 
